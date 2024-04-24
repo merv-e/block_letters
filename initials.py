17e881d52ed8e321bdbd2bea4f0d5116c89deed1
merv-e <34655454+merv-e@users.noreply.github.com>
2024-04-24 16:00:28 +0300

    initials of my name is created, as described (hopefully) in the exercise.

---
+++ initials.py
@@ -0,0 +1,21 @@
+# your first name and last name
+first_initial = "M"
+second_initial = "B"
+third_initial = "U"
+
+print(first_initial + "   " + first_initial )
+print(first_initial * 2 + " " + first_initial * 2)
+print(first_initial * 2 + " " + first_initial * 2)
+print(first_initial + " " + first_initial + " " + first_initial)
+print(first_initial + "   " + first_initial )
+print(first_initial + "   " + first_initial )
+print(first_initial + "   " + first_initial )
+
+# PS. This exercise is being cpmpleted in a different way. Maybe I am doing wrong :'(
+
+
+# A fun fact about yourself: I am still not a fluent Spanish speaker even after living in Spain
+# for almost more than 2 years.
+
+# TODO: Do the same with other initials.
+
